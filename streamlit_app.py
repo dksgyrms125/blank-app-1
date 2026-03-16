e6f3f464a026789a9d3e529f596fd7da328d956a
dksgyrms125 <dksgyrms1253@gmail.com>
2026-03-16 11:07:21 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(page_title="자기소개", page_icon="📝")
+
+st.title("안녕하세요, 저는 안효근입니다")
+
+st.markdown(
+    """
+    - **학번:** 202202980
+    - **출생년도:** 2000년생
+
+    저는 새로운 도전과 성장을 즐기며, 맡은 일을 책임감 있게 수행하는 것을 중요하게 생각합니다.
+    """
 )
